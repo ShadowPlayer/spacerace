f81d76dcb12014ee2ff67ace0dd2cca3cf75bd47
Brian Thorne <brian.thorne@nicta.com.au>
2015-07-29 19:32:55 +1000

    First cut at rendering with webgl with PIXI

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,5 +1,30 @@
 var io = require("io"); // Note socket.io is added globally in the html page
 var d3 = require("d3/d3.min.js");
+var PIXI = require("./pixi.min.js");
+
+var renderer;
+// create the root of the scene graph
+var stage = new PIXI.Container();
+
+// TODO replace with awesome ship...
+function rectangle( x, y, width, height, backgroundColor, borderColor, borderWidth ) {
+    var box = new PIXI.Graphics();
+    box.beginFill(backgroundColor);
+    box.lineStyle(borderWidth , borderColor);
+    box.drawRect(0, 0, width - borderWidth, height - borderWidth);
+    box.endFill();
+    box.position.x = x + borderWidth/2;
+    box.position.y = y + borderWidth/2;
+    return box;
+}
+
+var ships3D;
+
+// test
+// var r = rectangle(200, 150, 10, 10, 0xFFFFFF, 0x000000, 1);
+// create a 10x10 white rectangle with a 1px black border at position 200, 150
+//stage.addChild(r);
+
 
 require("./style.css");
 
@@ -18,7 +43,7 @@ socket.on('Log', function (msg) {
     var rawPacket = JSON.parse(msg);
     //console.log(rawPacket);
 
-    if(rawPacket.category === "lobby"){
+    if (rawPacket.category === "lobby") {
         console.log("Lobby Status received");
         console.log(rawPacket);
 
@@ -32,7 +57,9 @@ socket.on('Log', function (msg) {
 
         lobby.enter()
             .append("li")
-            .text(function(d, i){ return d; })
+            .text(function (d, i) {
+                return d;
+            })
 
 
     }
@@ -66,46 +93,11 @@ socket.on('GameState', function (msg) {
 //    setupGame(data)
 //}
 
-var svgContainer = d3.select('#game')
+var gameDiv = d3.select('#game')
     .attr("width", "100%")
     .attr("height", "500");
 
-var mapContainer = svgContainer.append("g");
 
-var shipG = svgContainer.append("defs")
-    .append("symbol")
-    .attr("viewBox", "0 0 50 50")
-    .attr("id", "ship")
-    .attr("class", "ship");
-
-shipG.append("polygon")
-    .attr("points", "25,2 47,37 3,37")
-    .attr("class", "shipWings");
-
-// For now the jet "backwards" is simply a solid line
-shipG.append("line")
-    .attr("class", "shipJet")
-    .attr("x1", 25)
-    .attr("y1", 35)
-    .attr("x2", 25)
-    .attr("y2", 45);
-
-// Main circular body of the ship
-shipG.append("circle")
-    .attr("class", "shipBody")
-    .attr('cx', 25)
-    .attr('cy', 25)
-    .attr('r', function (d, i) {
-        return 16;
-    });
-
-
-shipG.append("polygon")
-    .attr("points", "23,30 27,30 25,5")
-    .attr("class", "shipStripe");
-
-
-var shipGroup = svgContainer.append("g").attr("id", "shipsParentGroup");
 
 var ships;
 
@@ -131,24 +123,47 @@ function loadMap() {
         console.log("Loaded map image. Size = (%s, %s)", width, height);
 
         // Set the game's height to match the map's aspect ratio?
-        var actualWidth = parseInt(svgContainer.style("width"), 10);
+        // TODO we want to take width and height of the browser into account
+
+        var actualWidth = parseInt(gameDiv.style("width"), 10);
         var actualHeight = actualWidth / aspectRatio;
 
-        svgContainer
-            .attr("height", actualHeight);
 
-        // Add an <image> to our <svg>
-        // TODO test performance of putting it in a div behind the svg?
-        mapContainer.append("image")
-            .attr("id", "GameMap")
-            .attr("width", actualWidth)
-            .attr("height", actualHeight)
-            .attr("xlink:href", mapData);
+        console.log("Game size = ", actualWidth, actualHeight);
+
+        // Add an <image> to our game
+        // TODO add map image back to pixi canvas thing
+        //mapContainer.append("image")
+        //    .attr("id", "GameMap")
+        //    .attr("width", actualWidth)
+        //    .attr("height", actualHeight)
+        //    .attr("xlink:href", mapData);
+
+        renderer = PIXI.autoDetectRenderer(actualWidth, actualHeight, {backgroundColor : 0x000000});
+        document.getElementById("game").appendChild(renderer.view);
+
+
+        PIXI.loader
+            .add(mapData)
+            .load(function(){
+                console.log("Loaded map texture");
+                // https://github.com/kittykatattack/learningPixi#loading
+
+                // Probably don't want the map as a sprite...
+                // But this is a rough first cut...
+
+                var sprite = new PIXI.Sprite.fromImage(mapData);
+                stage.addChild(sprite);
+                renderer.render(stage)
+            });
+
 
         // Assume positions are between 0 and 100 for now
         // (0,0) is at the bottom left
         x = d3.scale.linear().domain([0, 100]).range([0, actualWidth]);
         y = d3.scale.linear().domain([0, 100]).range([actualHeight, 0]);
+
+        console.log("The stage is set");
 
     });
     mapImage.src = mapData;
@@ -162,12 +177,14 @@ var fps = d3.select("#fps span");
 
 var setupGame = function () {
     var initState = gameState;
-    var SHIPSIZE = "10";
+    var SHIPSIZE = 10;
 
-
-    initState.map(function(d){
-
-        d.color = "hsl(" + Math.random() * 360 + ",75%, 50%)";
+    // Come up with a random color for each ship
+    initState.map(function (d) {
+        //d.color = "hsl(" + Math.random() * 360 + ",75%, 50%)";
+        d.color = '#'+'0123456789ABCDEF'.split('').map(function(v,i,a){
+                return i>5 ? null : a[Math.floor(Math.random()*16)] }).join('');
+        d.colorAsInt = parseInt('0x' + d.color.slice(1));
     });
 
     // Show each player
@@ -178,40 +195,29 @@ var setupGame = function () {
     players.exit().remove();
     players.enter().append("li").append("button")
         .attr("title", "Click to select")
-        .on("click", function(d, i){
+        .on("click", function (d, i) {
             console.log("Selecting ship for player " + d.id);
             selectedShip = i;
         })
-        .style("color", function(d, i){
+        .style("color", function (d, i) {
             return d.color;
         })
-        .text(function(d, i){
+        .text(function (d, i) {
             return d.id;
         });
 
-
-    ships = shipGroup
-        .selectAll('.ship')
-        .data(initState)
-        .enter()
-        .append('use')
-        .attr("transform", function (d, i) {
-            return "translate(0, 0)";
-        })
-        .attr("id", function (d, i) {
-            return "ship" + i;
-        })
-        .attr("xlink:href", "#ship")
-        .attr("width", SHIPSIZE)
-        .attr("height", SHIPSIZE)
-        .attr('fill', function (d) {
-            return d.color;
-        });
+    ships3D = initState.map(function(d){
+        var r = rectangle(200, 150, SHIPSIZE, SHIPSIZE, d.colorAsInt, 0x000000, 1);
+        stage.addChild(r);
+        return r;
+    });
 
     // Trigger the first full draw
     updateState();
 
 };
+
+
 
 var updateState = function (highResTimestamp) {
 
@@ -230,42 +236,18 @@ var updateState = function (highResTimestamp) {
         lastUpdateTime = highResTimestamp;
 
 
-        ships
-            .data(gameState)
+        for (var i = 0; i < ships3D.length; i++) {
+            var cube = ships3D[i];
+            var d = gameState[i];
 
-            .style("fill", function(d, i){
-                if(selectedShip === i){
-                    return "black";
-                }
-            })
+            cube.rotation = (90 - d.theta * 360 / (2 * Math.PI));
+            cube.position.x = x(d.x);
+            cube.position.y = y(d.y);
+        }
 
-            // Perhaps we can add/remove the jet with display="none"
-            //.attr('opacity', function (d, i) {
-            //    // Show accelerating with opacity toggle
-            //    var accelerating = d.Tl == 1;
-            //    return accelerating ? 1.0 : 0.2;
-            //})
-            .attr("x", function (d, i) {
-                return x(d.x);
-            })
-            .attr("y", function (d, i) {
-                return y(d.y);
-            })
-            .attr("transform", function (d, i) {
-                // It is possible to both update ships' position and rotation using a single SVG transform/rotation
-                // command however it doesn't seem as performant as using the x,y attributes as above.
-                // Note SVG rotate takes degrees not radians, and it also takes positon (X, Y)
-                // to center the rotation around.
-                var shipX = x(d.x),
-                    shipY = y(d.y);
+        // render the container
+        renderer.render(stage);
 
-                return "rotate(" +
-                    (90 - d.theta * 360 / (2 * Math.PI)) +
-                    "," + shipX + ", " + shipY +
-                    ")";
-            });
     }
 };
-
-
 
